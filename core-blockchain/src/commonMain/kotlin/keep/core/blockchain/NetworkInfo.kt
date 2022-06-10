6a848408a27e402339ccb46106e886b18185b3a9
riatoshi <107013635+riatoshi@users.noreply.github.com>
2022-06-10 22:22:32 +0700

    Create network info

--- core-blockchain/src/commonMain/kotlin/keep/core/blockchain/NetworkInfo.kt
+++ core-blockchain/src/commonMain/kotlin/keep/core/blockchain/NetworkInfo.kt
@@ -1,12 +1,12 @@
 package keep.core.blockchain
 
-import keep.core.model.Account
 import keep.core.model.Address
 import keep.core.model.Asset
+import keep.core.model.Chain
 import keep.core.model.TokenStandard
 
-sealed class Chain(
-    val chain: keep.core.model.Chain,
+sealed class NetworkInfo(
+    val chain: Chain,
     val defaultChainId: String = "0",
     val meta: TxMetaType = TxMetaType.NONE,
     val feeType: FeeType = FeeType.PASSIVE,
@@ -72,22 +72,22 @@ sealed class Chain(
     )
 }
 
-object Binance : Chain(
-    chain = keep.core.model.Chain.Binance,
+object Binance : NetworkInfo(
+    chain = Chain.Binance,
     meta = TxMetaType.MEMO,
     defaultChainId = "Binance-Chain-Tigris",
 )
 
-private val chains: Array<Chain> = arrayOf(
+private val chains: Array<NetworkInfo> = arrayOf(
     Binance,
 )
 
-fun findNetworkBy(coinType: Int): Chain {
+fun findNetworkBy(coinType: Int): NetworkInfo {
     return chains.firstOrNull{ it.coinType == coinType}
         ?: throw IllegalArgumentException("Coin type: ${coinType} is not supported")
 }
 
-fun findNetworkBy(networkId: String): Chain {
+fun findNetworkBy(networkId: String): NetworkInfo {
     return chains.firstOrNull{ it.networkId == networkId}
         ?: throw IllegalArgumentException("Network Id: ${networkId} is not supported")
 }
