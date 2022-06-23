6314b235b061c728510c6699c52f0da0b517fe10
riatoshi <107013635+riatoshi@users.noreply.github.com>
2022-06-23 11:03:21 +0700

    Add fake

--- core-crypto/src/commonMain/kotlin/keep/core/crypto/impl/FakeWalletAdapter.kt
+++ core-crypto/src/commonMain/kotlin/keep/core/crypto/impl/FakeWalletAdapter.kt
@@ -6,20 +6,20 @@ import keep.core.crypto.model.Seed
 import keep.core.crypto.model.SignInput
 import keep.core.crypto.model.SignOutput
 import keep.core.model.Account
+import keep.core.model.Address
 import keep.core.model.Chain
 
 class FakeWalletAdapter(
     private val entropyGenerator: EntropyGenerator = FakeEntropyGenerator()
 ) : WalletAdapter {
-    override suspend fun generateSeed(chains: List<Chain>): Seed {
-        TODO("Not yet implemented")
-    }
+    override suspend fun generateSeed(chains: List<Chain>): Seed
+        = Seed(entropyGenerator.generate(), "")
 
-    override suspend fun generateAccount(seed: Seed, chain: Chain, index: Int): Account {
-        TODO("Not yet implemented")
-    }
+    override suspend fun generateAccount(seed: Seed, chain: Chain, index: Int): Account
+        = Account(chain, 0, Address("0xabcdef123456789abc"))
 
-    override suspend fun sign(seed: Seed, input: SignInput): SignOutput {
-        TODO("Not yet implemented")
+    override suspend fun sign(seed: Seed, input: SignInput): SignOutput = object : SignOutput {
+        override val data: ByteArray
+            get() = byteArrayOf()
     }
 }
