bdf784416ac64cd103b10564df4f9d913dfeaca5
riatoshi <107013635+riatoshi@users.noreply.github.com>
2022-06-29 11:30:09 +0700

    Add colors

--- core-ui/src/commonMain/kotlin/keep/core/ui/theme/Colors.kt
+++ core-ui/src/commonMain/kotlin/keep/core/ui/theme/Colors.kt
@@ -1,7 +1,7 @@
 package keep.core.ui.theme
 
-val Red200 = 0xfff297a2
-val Red300 = 0xffea6d7e
-val Red700 = 0xffdd0d3c
-val Red800 = 0xffd00036
-val Red900 = 0xffc20029
+val Red200Val = 0xfff297a2
+val Red300Val = 0xffea6d7e
+val Red700Val = 0xffdd0d3c
+val Red800Val = 0xffd00036
+val Red900Val = 0xffc20029
