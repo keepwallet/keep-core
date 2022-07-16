9f42cf02b0126a4566d9f54c0427eb1b48068b76
riatoshi <107013635+riatoshi@users.noreply.github.com>
2022-07-16 21:19:11 +0700

    Clean import

--- feature-assets/src/main/java/keep/core/feature/assets/components/AssetList.kt
+++ feature-assets/src/main/java/keep/core/feature/assets/components/AssetList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import keep.core.feature.assets.model.AssetUIState
 import keep.core.feature.assets.model.AssetsHeadUIState
